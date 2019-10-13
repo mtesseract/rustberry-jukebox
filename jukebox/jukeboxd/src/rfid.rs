b7e3fb9af1bc0f75f172924e5f40cda02f8e462c
Moritz Clasmeier <mtesseract@silverratio.net>
2019-10-13 21:06:58 +0200

    WIP / testing

--- jukebox/jukeboxd/src/rfid.rs
+++ jukebox/jukeboxd/src/rfid.rs
@@ -103,8 +103,8 @@ impl Write for TagWriter {
         let n_to_skip = if self.current_pos_in_buffered_data > 0 {
             // Need to fill currently buffered data first.
             let n_space_left_in_buffered_data =
-                [self.current_pos_in_buffered_data as usize..N_BLOCK_SIZE as usize].len();
-            dbg!([self.current_pos_in_buffered_data as usize..N_BLOCK_SIZE as usize]);
+                (self.current_pos_in_buffered_data as usize..N_BLOCK_SIZE as usize).len();
+            dbg!((self.current_pos_in_buffered_data as usize..N_BLOCK_SIZE as usize));
             dbg!(n_space_left_in_buffered_data);
             let to_copy_into_buffered_data: u8 =
                 std::cmp::min(buf.len(), n_space_left_in_buffered_data) as u8;
