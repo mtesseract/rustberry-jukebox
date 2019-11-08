26876f6c39a9a5102a92a86506334b63969f8908
Moritz Clasmeier <mtesseract@silverratio.net>
2019-11-08 14:37:54 +0100

    WIP

--- jukeboxd/src/led_controller.rs
+++ jukeboxd/src/led_controller.rs
@@ -1,6 +1,5 @@
 use failure::Fallible;
-use slog_scope::{info,warn};
-
+use slog_scope::{info, warn};
 
 #[derive(Debug)]
 pub enum Error {
@@ -16,7 +15,6 @@ impl std::fmt::Display for Error {
 }
 
 impl std::error::Error for Error {}
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Led {
@@ -35,7 +33,10 @@ pub struct LedController {
 
 impl LedController {
     pub fn new<LCB: LedControllerBackend + Send + Sync + 'static>(backend: LCB) -> Fallible<Self> {
-        info!("Creating LED Controller using backend {}", backend.description());
+        info!(
+            "Creating LED Controller using backend {}",
+            backend.description()
+        );
         Ok(LedController {
             backend: Box::new(backend),
         })
@@ -58,7 +59,7 @@ pub mod backends {
     pub mod gpio_cdev {
         use super::super::{Error, Led, LedControllerBackend};
         use failure::Fallible;
-        use gpio_cdev::{Chip, LineRequestFlags};
+        use gpio_cdev::{Chip, LineHandle, LineRequestFlags};
         use serde::Deserialize;
         use slog_scope::{info, warn};
 
@@ -70,6 +71,7 @@ pub mod backends {
         pub struct GpioCdev {
             config: Config,
             chip: Chip,
+            playback_led: Option<LineHandle>,
         }
 
         impl GpioCdev {
@@ -78,9 +80,31 @@ pub mod backends {
                 if !led_to_line(&config, Led::Playback).is_some() {
                     warn!("No GPIO line configured for LED {:?}. Skipping all future requests for this LED.", Led::Playback);
                 }
-                let chip = Chip::new("/dev/gpiochip0")
+                let mut chip = Chip::new("/dev/gpiochip0")
                     .map_err(|err| Error::IO(format!("Failed to open Chip: {:?}", err)))?;
-                Ok(GpioCdev { chip, config })
+                let playback_led = match config.playback_led_gpio_line {
+                    Some(line) => {
+                        let output = chip.get_line(line).map_err(|err| {
+                            Error::IO(format!("Failed to get Playback LED GPIO line: {:?}", err))
+                        })?;
+                        let playback_led = output
+                            .request(LineRequestFlags::OUTPUT, 0, "led-gpio")
+                            .map_err(|err| {
+                                Error::IO(format!(
+                                    "Failed to request output handle for GPIO line: {:?}",
+                                    err
+                                ))
+                            })?;
+                        Some(playback_led)
+                    }
+                    None => None,
+                };
+
+                Ok(GpioCdev {
+                    chip,
+                    config,
+                    playback_led,
+                })
             }
         }
 
@@ -95,64 +119,29 @@ pub mod backends {
                 "gpio-cdev backend".to_string()
             }
             fn switch_on(&mut self, led: Led) -> Fallible<()> {
-                let line = match led_to_line(&self.config, led) {
-                    None => {
-                        return Ok(());
-                    }
-                    Some(line) => line,
-                };
-
-                let output = self
-                    .chip
-                    .get_line(line)
-                    .map_err(|err| Error::IO(format!("Failed to get GPIO line: {:?}", err)))?;
-                let output_handle = output
-                    .request(LineRequestFlags::OUTPUT, 0, "led-gpio")
-                    .map_err(|err| {
+                if let Some(ref playback_led) = self.playback_led {
+                    playback_led.set_value(1).map_err(|err| {
                         Error::IO(format!(
-                            "Failed to request output handle for GPIO line: {:?}",
+                            "Failed to activate Playback LED GPIO line, {:?}",
                             err
                         ))
                     })?;
-                output_handle.set_value(1).map_err(|err| {
-                    Error::IO(format!(
-                        "Failed to activate output GPIO line {}, {:?}",
-                        line, err
-                    ))
-                })?;
-                info!("Switched on GPIO line {} for LED {:?}", line, led);
+                    info!("Switched on GPIO line for LED {:?}", led);
+                }
                 Ok(())
             }
             fn switch_off(&mut self, led: Led) -> Fallible<()> {
-                let line = match led_to_line(&self.config, led) {
-                    None => {
-                        return Ok(());
-                    }
-                    Some(line) => line,
-                };
-
-                let output = self
-                    .chip
-                    .get_line(line)
-                    .map_err(|err| Error::IO(format!("Failed to get GPIO line: {:?}", err)))?;
-                let output_handle = output
-                    .request(LineRequestFlags::OUTPUT, 0, "led-gpio")
-                    .map_err(|err| {
+                if let Some(ref playback_led) = self.playback_led {
+                    playback_led.set_value(0).map_err(|err| {
                         Error::IO(format!(
-                            "Failed to request output handle for GPIO line: {:?}",
+                            "Failed to deactivate Playback LED GPIO line {:?}",
                             err
                         ))
                     })?;
-                output_handle.set_value(0).map_err(|err| {
-                    Error::IO(format!(
-                        "Failed to activate output GPIO line {}, {:?}",
-                        line, err
-                    ))
-                })?;
-                info!("Switched off GPIO line {} for LED {:?}", line, led);
+                    info!("Switched off GPIO line for LED {:?}", led);
+                }
                 Ok(())
             }
         }
-
     }
 }
