541c21f09fa32f7c4a04bfb600f77ca453eddf4b
Moritz Clasmeier <mtesseract@silverratio.net>
2019-12-15 22:53:17 +0100

    Test RFID

--- jukeboxd/src/bin/rustberry-rfid.rs
+++ jukeboxd/src/bin/rustberry-rfid.rs
@@ -1,15 +1,53 @@
 use failure::Fallible;
-use std::io::Read;
+use slog::{self, o};
+use slog_scope::{error, info, warn};
 
+use rustberry::playback_requests::*;
 use rustberry::rfid::*;
 
-fn main() -> Fallible<()> {
-    let mut rc = RfidController::new()?;
-    let tag = rc.open_tag()?.unwrap();
-    println!("{:?}", tag.uid);
-    let mut buf: [u8; 3] = [0; 3];
+fn handle_tag(tag: Tag) -> Fallible<()> {
     let mut tag_reader = tag.new_reader();
-    tag_reader.read_exact(&mut buf).unwrap();
-    println!("read: {:?}", buf);
+    let request_string = tag_reader.read_string()?;
+    let request_deserialized: PlaybackRequest = serde_json::from_str(&request_string)?;
+    info!("Request: {:?}", request_deserialized);
     Ok(())
+}
+
+fn run() -> Fallible<()> {
+    let mut rc = RfidController::new()?;
+    let mut last_uid: Option<String> = None;
+    loop {
+        match rc.open_tag() {
+            Err(err) => {
+                // Do not change playback state in this case.
+                warn!("Failed to open RFID tag: {}", err);
+            }
+            Ok(None) => {
+                if last_uid.is_some() {
+                    info!("RFID Tag gone");
+                    last_uid = None;
+                }
+            }
+            Ok(Some(tag)) => {
+                let current_uid = format!("{:?}", tag.uid);
+                if last_uid == Some(current_uid.clone()) {
+                    std::thread::sleep(std::time::Duration::from_millis(1500));
+                    continue;
+                }
+                // new tag!
+                match handle_tag(tag) {
+                    Ok(_) => {
+                        last_uid = Some(current_uid);
+                    }
+                    Err(err) => {
+                        error!("Failed to handle tag: {}", err);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() -> Fallible<()> {
+    slog_scope::scope(&slog_scope::logger().new(o!()), || run())
 }
