78be66b10ce07c8475145dd2e970eac76e7fda0e
Moritz Clasmeier <mtesseract@silverratio.net>
2020-03-10 22:12:05 +0100

    WIP

--- jukeboxd/src/effects/spotify/player.rs
+++ jukeboxd/src/effects/spotify/player.rs
@@ -75,6 +75,7 @@ impl SpotifyPlayer {
             None => return Err(Error::NoSpotifyDevice),
         };
         dbg!(&device_id);
+        dbg!(&access_token);
         let req = Self::derive_start_playback_payload_from_spotify_uri(spotify_uri);
         self.http_client
             .put("https://api.spotify.com/v1/me/player/play")
