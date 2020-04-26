9713d905cba1b12b3744bef7267a39a18dbf93ee
Moritz Clasmeier <mtesseract@silverratio.net>
2020-04-26 23:00:57 +0200

    wip

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -3,3 +3,11 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
+
+source ~/.cargo/env
+export OPENSSL_LIB_DIR=/usr/local/openssl
+export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
+export OPENSSL_LIB_DIR=/usr/local/openssl
+export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
+export PKG_CONFIG_ALLOW_CROSS=1
+cargo build --release --bin jukeboxd --target=armv7-unknown-linux-gnueabihf
