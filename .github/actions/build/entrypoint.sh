53fd66a5b541166929d6c9982e141ac79acaa531
Moritz Clasmeier <mtesseract@silverratio.net>
2020-04-29 10:44:58 +0200

    wip

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -15,11 +15,14 @@ export PATH="/root/.cargo/bin:$PATH"
 export CARGO_HOME=/root/.cargo
 export RUSTUP_HOME=/root/.rustup
 
+ln -sf /github/home/caches/registry $CARGO_HOME/registry
+# ln -sf /github/home/caches/target /github/workspace/jukeboxd/target
+
 ls -lh $CARGO_HOME/
 ls -lh $CARGO_HOME/registry
 
 cd jukeboxd
 # cargo build-deps --release  --target=armv7-unknown-linux-gnueabihf
 cargo build --release --bin jukeboxd --target=armv7-unknown-linux-gnueabihf
-cp -r target target-deps
+# cp -r target target-deps
 #cargo build --release --bin jukeboxd --target=armv7-unknown-linux-gnueabihf
