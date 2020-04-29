e78c865e2c4f0770ed076e6bc9a84dbdbb177e88
Moritz Clasmeier <mtesseract@silverratio.net>
2020-04-29 21:03:35 +0200

    Generalize Architecture

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 
-set -x
+TARGET_ARCH="$1"
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+echo "Building inside container..."
 
 export OPENSSL_LIB_DIR=/usr/local/openssl
 export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
@@ -15,11 +13,16 @@ export PATH="/root/.cargo/bin:$PATH"
 export CARGO_HOME=/root/.cargo
 export RUSTUP_HOME=/root/.rustup
 
+# Delete Cargo caches shipped with the container
+
 rm -rf $CARGO_HOME/registry
 rm -rf $CARGO_HOME/git
 
+# And replace them with those coming from the GitHub Action environment.
 ln -sf /github/home/caches/registry $CARGO_HOME/registry
 ln -sf /github/home/caches/git $CARGO_HOME/git
 
 cd jukeboxd
-cargo build --release --bin jukeboxd --target=armv7-unknown-linux-gnueabihf
+cargo build --release --bin jukeboxd --target=$TARGET_ARCH
+mkdir _artifacts
+cp jukeboxd/target/$TARGET_ARCH/release/jukeboxd _artifacts
