a8cdb7ac43f21d039a671e3683875552ae344092
Moritz Clasmeier <mtesseract@silverratio.net>
2020-04-29 07:32:12 +0200

    wip

--- .github/actions/build/entrypoint.sh
+++ .github/actions/build/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-set -e
 set -x
 
 echo "Hello $1"
@@ -15,6 +14,9 @@ export PKG_CONFIG_ALLOW_CROSS=1
 export PATH="/root/.cargo/bin:$PATH"
 export CARGO_HOME=/root/.cargo
 export RUSTUP_HOME=/root/.rustup
+
+ls -lh $CARGO_HOME/
+ls -lh $CARGO_HOME/registry
 
 cd jukeboxd
 # cargo build-deps --release  --target=armv7-unknown-linux-gnueabihf
