646aa691fbe52cead0ebbabae54f7d5cef22a4fc
Moritz Clasmeier <mtesseract@silverratio.net>
2020-04-30 23:43:43 +0200

    testing

---
+++ .github/actions/build/armv7-unknown-linux-musleabihf/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -l
+
+echo "Building inside container..."
+
+export OPENSSL_LIB_DIR=/usr/local/openssl
+export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
+export OPENSSL_LIB_DIR=/usr/local/openssl
+export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
+export PKG_CONFIG_ALLOW_CROSS=1
+export PATH="/root/.cargo/bin:$PATH"
+export CARGO_HOME=/root/.cargo
+export RUSTUP_HOME=/root/.rustup
+
+# Delete Cargo caches shipped with the container
+
+rm -rf $CARGO_HOME/registry
+rm -rf $CARGO_HOME/git
+
+# And replace them with those coming from the GitHub Action environment.
+ln -sf /github/home/caches/registry $CARGO_HOME/registry
+ln -sf /github/home/caches/git $CARGO_HOME/git
+
+cd jukeboxd
+cargo build --release --bin jukeboxd --target=$TARGET_ARCH
+mkdir _artifacts
+cp target/$TARGET_ARCH/release/jukeboxd _artifacts
