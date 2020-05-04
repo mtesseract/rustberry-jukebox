da2a015f62151186641b84f8cee7e90bd645da4c
Moritz Clasmeier <mtesseract@silverratio.net>
2020-05-04 19:18:55 +0200

    WIP

--- .github/actions/build/arm-unknown-linux-gnueabihf/entrypoint.sh
+++ .github/actions/build/arm-unknown-linux-gnueabihf/entrypoint.sh
@@ -2,17 +2,9 @@
 
 echo "Building inside container..."
 
-export OPENSSL_LIB_DIR=/usr/local/openssl
-export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
-export OPENSSL_LIB_DIR=/usr/local/openssl
-export OPENSSL_INCLUDE_DIR=/usr/local/openssl/include
 export PKG_CONFIG_ALLOW_CROSS=1
-export PATH="/root/.cargo/bin:$PATH"
-export CARGO_HOME=/root/.cargo
-export RUSTUP_HOME=/root/.rustup
 
 # Delete Cargo caches shipped with the container
-
 rm -rf $CARGO_HOME/registry
 rm -rf $CARGO_HOME/git
 
@@ -20,10 +12,7 @@ rm -rf $CARGO_HOME/git
 ln -sf /github/home/caches/registry $CARGO_HOME/registry
 ln -sf /github/home/caches/git $CARGO_HOME/git
 
-ln -s $(which $CC) /usr/bin/cc
-ls -l /usr/bin/cc
-echo "PATH: $PATH"
-
+# Build it
 cd jukeboxd
 cargo build --release --bin jukeboxd --target=arm-unknown-linux-gnueabihf
 mkdir _artifacts
