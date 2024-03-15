fac3995b2a62f05ecca0b86395fe59f9cd14e0c6
Moritz Clasmeier <mtesseract@silverratio.net>
2024-03-15 14:31:12 +0100

    wip

--- src/config.rs
+++ src/config.rs
@@ -6,10 +6,20 @@ pub struct Config {
     // pub client_id: Option<String>,
     // pub client_secret: Option<String>,
     // pub device_name: Option<String>,
+    #[serde(default = "enable_spotify_default")]
     pub enable_spotify: bool,
     pub post_init_command: Option<String>,
     pub shutdown_command: Option<String>,
     pub volume_up_command: Option<String>,
     pub volume_down_command: Option<String>,
+    #[serde(default = "trigger_only_mode_default")]
     pub trigger_only_mode: bool,
+}
+
+func trigger_only_mode_default() -> bool {
+    true
+}
+
+func enable_spotify_default() -> bool {
+    false
 }
