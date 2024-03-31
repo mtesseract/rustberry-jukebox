2d483dbf27c6bc002436b5bb9cdf20093e6934dd
Moritz Clasmeier <mtesseract@silverratio.net>
2024-03-31 19:24:57 +0000

    wip

--- src/input_controller/playback.rs
+++ src/input_controller/playback.rs
@@ -1,20 +1,9 @@
 use anyhow::{Context, Result};
 use crossbeam_channel::{self, Receiver, Sender};
-use slog_scope::{error, info, warn};
+use slog_scope::{error, info, trace, warn};
 use std::{thread, time::Duration};
 
-use crate::player::{PlaybackRequest};
-
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-//     #[test]
-//     fn test_user_request_spotify_uri_serialization() {
-//         let user_req = PlaybackResource::SpotifyUri("foo".to_string());
-//         let serialized = serde_json::to_string(&user_req).unwrap();
-//         assert_eq!(serialized, "{\"SpotifyUri\":\"foo\"}".to_string());
-//     }
-// }
+use crate::player::PlaybackRequest;
 
 pub struct Handle<T> {
     channel: Receiver<T>,
@@ -64,7 +53,7 @@ pub mod rfid {
                         thread::sleep(Duration::from_millis(80));
                     }
                     Ok(None) => {
-                        info!("No PICC seen.");
+                        trace!("No PICC seen.");
                         if last_uid.is_some() {
                             info!("PICC gone");
                             last_uid = None;
@@ -75,14 +64,13 @@ pub mod rfid {
                         }
                     }
                     Ok(Some(tag)) => {
-                        info!("Seen PICC {:?}", tag);
-                        
+                        trace!("Seen PICC {:?}", tag);
+
                         let tagclone = tag.clone();
 
                         let current_uid = tag.uid;
                         if last_uid != Some(current_uid.clone()) {
-                            // new tag!
-                            info!("Seen new tag {}", current_uid);
+                            info!("Seen new PICC {}", current_uid);
                             if let Err(err) = self.tx.send(PlaybackRequest::Start(tagclone).into()) {
                                 error!("Failed to handle tag: {}", err);
                                 thread::sleep(Duration::from_millis(80));
@@ -97,55 +85,3 @@ pub mod rfid {
         }
     }
 }
-
-// pub mod stdin {
-//     use super::*;
-
-//     pub struct PlaybackRequestTransmitterStdin<T> {
-//         tx: Sender<Option<T>>,
-//     }
-
-//     impl<T: DeserializeOwned + Clone + std::fmt::Debug + PartialEq> PlaybackRequestTransmitterStdin<T> {
-//         pub fn new<F>(msg_transformer: F) -> Result<Handle<T>>
-//         where
-//             F: Fn(PlaybackRequest) -> Option<T> + 'static + Send + Sync,
-//         {
-//             let (tx, rx) = crossbeam_channel::bounded(1);
-//             let transmitter = Self { tx };
-//             transmitter.run(msg_transformer)?;
-//             Ok(Handle { channel: rx })
-//         }
-
-//         fn run<F>(&self, msg_transformer: F) -> Result<()>
-//         where
-//             F: Fn(PlaybackRequest) -> Option<T> + 'static + Send,
-//         {
-//             let mut last: Option<PlaybackRequest> = None;
-
-//             let stdin = std::io::stdin();
-//             for line in stdin.lock().lines() {
-//                 if let Ok(ref line) = line {
-//                     let req: Option<PlaybackRequest> = if line == "" {
-//                         None
-//                     } else {
-//                         match serde_json::from_str(line) {
-//                             Ok(deserialized) => Some(deserialized),
-//                             Err(err) => {
-//                                 error!("Failed to deserialize line `{}`: {}", line, err);
-//                                 None
-//                             }
-//                         }
-//                     };
-//                     if last != req {
-//                         if let Some(transformed_req) = req.clone().and_then(&msg_transformer) {
-//                             self.tx.send(Some(transformed_req)).unwrap();
-//                         }
-//                         last = req;
-//                     }
-//                 }
-//             }
-
-//             panic!();
-//         }
-//     }
-// }
